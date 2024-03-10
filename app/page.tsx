4607a01309464b3a67f0618768b6c8b514ee5790
Akash Saini <akashsaini0020@gmail.com>
2024-03-10 22:20:15 +0530

    hiding some information

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Intro from "@/components/Intro";
 import About from "@/components/About";
-import Projects from "@/components/Projects";
+// import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
-import Experience from "@/components/Experience";
+// import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 
 export default function Home() {
@@ -10,9 +10,9 @@ export default function Home() {
     <main className="flex flex-col items-center px-4 ">
      <Intro/>
      <About/>
-     <Projects/>
+     {/* <Projects/> */}
      <Skills/>
-     <Experience/>
+     {/* <Experience/> */}
      <Contact/>
     </main>
   );
